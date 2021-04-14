4a81694e530dafbc9ff0efd03be26adb315cd55f
vtsvirko <48400528+vtsvirko@users.noreply.github.com>
2021-04-13 20:21:59 -0700

    files related to menu.html page

---
+++ javaliscious/menu/menu.js
@@ -0,0 +1,23 @@
+async function renderCards() {
+    const response = await fetch('/menu.json');
+    const menu = await response.json();  
+
+    document.getElementById("menu").innerHTML = `
+   
+    ${menu.map(function(item) {
+        return ` 
+        <div class="card h-200" style="width: 18rem">
+        <a href="${item.url}">
+            <img class="card-img-top" src="${item.image_url}" alt="">
+        
+            <div class="card-body">
+                <p>${item.name}</p>
+                <li>${item.description}</li>
+            </div>
+        </div>
+        </a>
+        ` 
+}).join('')}
+`
+}
+renderCards();
